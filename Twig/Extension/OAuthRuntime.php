337a85656c26d333fd852a07b5f1b9b03896114a
David Maicher <mail@dmaicher.de>
2021-07-05 18:56:01 +0200

    make twig extension lazy by using a Runtime

---
+++ Twig/Extension/OAuthRuntime.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the HWIOAuthBundle package.
+ *
+ * (c) Hardware Info <opensource@hardware.info>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HWI\Bundle\OAuthBundle\Twig\Extension;
+
+use HWI\Bundle\OAuthBundle\Templating\Helper\OAuthHelper;
+use Twig\Extension\RuntimeExtensionInterface;
+
+final class OAuthRuntime implements RuntimeExtensionInterface
+{
+    /**
+     * @var OAuthHelper
+     */
+    private $helper;
+
+    public function __construct(OAuthHelper $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    /**
+     * @return array
+     */
+    public function getResourceOwners()
+    {
+        return $this->helper->getResourceOwners();
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    public function getLoginUrl($name)
+    {
+        return $this->helper->getLoginUrl($name);
+    }
+
+    /**
+     * @param string $name
+     * @param string $redirectUrl     Optional
+     * @param array  $extraParameters Optional
+     *
+     * @return string
+     */
+    public function getAuthorizationUrl($name, $redirectUrl = null, array $extraParameters = [])
+    {
+        return $this->helper->getAuthorizationUrl($name, $redirectUrl, $extraParameters);
+    }
+}
